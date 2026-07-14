e3cdb12a190e5bd379d3b91b08a14fa1d6d2d594
Shivam Anand <optimusedprime@gmail.com>
2026-07-14 16:50:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+# Configuration file
+
+# Path to your dataset
+DATA_PATH = "data/Toronto Island Ferry Tickets.csv"
+
+# Forecast horizons in minutes (15m, 30m, 1h, 2h)
+FORECAST_HORIZONS = [15, 30, 60, 120]
 
