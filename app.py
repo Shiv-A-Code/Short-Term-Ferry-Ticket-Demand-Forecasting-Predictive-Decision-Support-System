bf401b9e72312c65fc0008129b266f3377297005
Shivam Anand <optimusedprime@gmail.com>
2026-07-16 16:52:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from model_utils import prepare_data, naive_forecast, random_forest_forecast, pr
 from evaluation import kpi_summary
 from config import DATA_PATH
 
-st.title("🚢 Ferry Ticket Demand Forecasting Dashboard")
+st.title("Ferry Ticket Demand Forecasting Dashboard")
 
 # Sidebar controls
 model_choice = st.sidebar.selectbox("Select Model", ["Naive", "Random Forest", "Prophet"])
@@ -18,14 +18,14 @@ if model_choice == "Naive":
     forecast_df = naive_forecast(df, horizon=horizon//15)
     st.line_chart(forecast_df[['Sales Count','predicted']])
     summary = kpi_summary(forecast_df['Sales Count'].dropna(), forecast_df['predicted'].dropna(), horizon=horizon//15)
-    st.write("### 📊 Metrics & KPIs")
+    st.write("### Metrics & KPIs")
     st.table(summary)
 
 elif model_choice == "Random Forest":
     forecast_df = random_forest_forecast(df, horizon=horizon//15)
     st.line_chart(forecast_df[['Sales Count','predicted']])
     summary = kpi_summary(forecast_df['Sales Count'], forecast_df['predicted'], horizon=horizon//15)
-    st.write("### 📊 Metrics & KPIs")
+    st.write("###Metrics & KPIs")
     st.table(summary)
 
 elif model_choice == "Prophet":
@@ -33,7 +33,7 @@ elif model_choice == "Prophet":
     st.line_chart(forecast_df[['ds','yhat']].set_index('ds'))
     st.area_chart(forecast_df[['ds','yhat_lower','yhat_upper']].set_index('ds'))
     summary = kpi_summary(df['Sales Count'], forecast_df['yhat'], forecast_df, horizon=horizon//15)
-    st.write("### 📊 Metrics & KPIs")
+    st.write("###Metrics & KPIs")
     st.table(summary)
-    st.download_button("⬇️ Download KPI Summary", summary.to_csv().encode('utf-8'), "kpi_summary.csv", "text/csv")
+    st.download_button("Download KPI Summary", summary.to_csv().encode('utf-8'), "kpi_summary.csv", "text/csv")
 
